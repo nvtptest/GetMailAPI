ac49101e20768d13c41129a481aa46b0d867707d
Phong Nguyen <phong1421@outlook.com>
2022-07-31 09:09:58 +0700

    Commit

---
+++ RunAppWindowService.js
@@ -0,0 +1,16 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'GetMailAPI',
+  description: 'The nodejs.org get mail API.',
+  script: 'D:\\Bravo\\Other_Project\\GetMailListener2\server.js'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
